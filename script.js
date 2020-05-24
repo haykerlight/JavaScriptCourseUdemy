f9ca1fa1c33bbcb6a1ce53178aecd0ab271a539d
Boriss <example@gmail.com>
2020-05-24 14:12:44 +0300

    lesson 3

--- script.js
+++ script.js
@@ -1,57 +1,24 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let num = 50;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''), 
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
-
-
-if (4 == 9) {
-    console.log('Ok');
-} else {
-    console.log('Error');
-}
-
-
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('too much');
-// } else {
-//     console.log('ok!');
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-// (num === 50) ? console.log('ok!') : console.log('Error');
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
 
-const num = 50;
-
-switch (num) {
-    case 49: 
-        console.log('No');
-        break;
-    case 100:
-        console.log('No');
-        break;
-    case 50:
-        console.log('Yes');
-        break;
-    default:
-        console.log('meh');
-        break;
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    
+    console.log(i);
+    
 }
