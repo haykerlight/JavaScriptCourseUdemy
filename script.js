8875e43e749150b4352482112a4183a051da77a3
Boriss <example@gmail.com>
2020-05-24 15:13:23 +0300

    practical part 2

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''), 
+          b = prompt('На сколько оцените его?', '');
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    
-    console.log(i);
-    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }       
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман!");
+} else {
+    console.log("Произошла ошибка!");
+}
+
+
+console.log(personalMovieDB);
